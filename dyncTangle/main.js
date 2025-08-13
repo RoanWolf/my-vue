8b135362980c3d6d86dff62d23222e7f2d89f9ba
neq <neq17320251@gmail.com>
2025-08-14 00:11:08 +0800

    练习3完成

--- dyncTangle/main.js
+++ dyncTangle/main.js
@@ -7,3 +7,33 @@
 // {
 //   transform: `perspective(0px)rotateX(0deg)rotateY(0deg)rotateZ(0deg)`,
 // }
+const { createApp } = Vue;
+
+const app = {
+  data() {
+    return {
+      x: 0,
+      y: 0,
+      z: 0,
+      size: 500,
+    };
+  },
+  watch: {},
+  methods: {
+    reset() {
+      this.x = 0;
+      this.y = 0;
+      this.z = 0;
+      this.size = 500;
+    },
+  },
+  computed: {
+    transformStyle() {
+       return {
+        transform: `perspective(${this.size}px)rotateX(${this.x}deg)rotateY(${this.y}deg)rotateZ(${this.z}deg)`
+       }
+    },
+  },
+};
+
+createApp(app).mount("#app");
