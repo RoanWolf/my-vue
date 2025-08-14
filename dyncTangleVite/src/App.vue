248db3e45a83fd3e5e44aca028455ca721f0cb8d
neq <neq17320251@gmail.com>
2025-08-14 15:35:00 +0800

    练习5 完成

---
+++ dyncTangleVite/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <h2>CSS3 Perspective Playground</h2>
+  <main>
+    <Settings
+      :x="x"
+      @update-x="x = Number($event)"
+      :y="y"
+      @update-y="y = Number($event)"
+      :z="z"
+      @update-z="z = Number($event)"
+      :size="size"
+      @update-size="size = Number($event)"
+    />
+    <section class="output">
+      <div class="box-container" x>
+        <!-- Set style -->
+        <div class="box" :style="transformStyle"></div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+import Settings from "./components/Settings.vue";
+
+/*
+  1. Sync input value with vue data and show it on the label(v-model 2-ways binding)
+  2. Implement reset(v-on and methods)
+  3. Apply transform style to the cube(computed)
+*/
+
+// {
+//   transform: `perspective(0px)rotateX(0deg)rotateY(0deg)rotateZ(0deg)`,
+// }
+
+const app = {
+  components: { Settings },
+  data() {
+    return {
+      x: 0,
+      y: 0,
+      z: 0,
+      size: 500,
+    };
+  },
+  watch: {},
+  methods: {},
+  computed: {
+    transformStyle() {
+      return {
+        transform: `perspective(${this.size}px)rotateX(${this.x}deg)rotateY(${this.y}deg)rotateZ(${this.z}deg)`,
+      };
+    },
+  },
+};
+
+export default app;
+</script>
+
+<style></style>
---
+++ dyncTangleVite/src/components/Settings.vue
@@ -0,0 +1,82 @@
+<template>
+  <section class="settings">
+    <div class="settings-container">
+      <label>perspective: {{ size }} px;</label>
+      <input
+        title="perspective"
+        type="range"
+        min="0"
+        max="999"
+        :value="size"
+        @input="updateSize"
+      />
+
+      <label>rotateX: {{ x }} deg; </label>
+      <input
+        title="rotateX"
+        type="range"
+        min="-180"
+        max="180"
+        :value="x"
+        @input="updateX"
+      />
+
+      <label>rotateY: {{ y }} deg; </label>
+      <input
+        title="rotateY"
+        type="range"
+        min="-180"
+        max="180"
+        :value="y"
+        @input="updateY"
+      />
+
+      <label>rotateZ: {{ z }} deg; </label>
+      <input
+        title="rotateZ"
+        type="range"
+        min="-180"
+        max="180"
+        :value="z"
+        @input="updateZ"
+      />
+
+      <button type="button" @click="reset">Reset</button>
+    </div>
+  </section>
+</template>
+
+<script>
+const obj = {
+  props: {
+    x: Number,
+    y: Number,
+    z: Number,
+    size: Number,
+  },
+  methods: {
+    reset() {
+      this.$emit("update-size", 500);
+      this.$emit("update-x", 0);
+      this.$emit("update-y", 0);
+      this.$emit("update-z", 0);
+    },
+    updateSize(event) {
+      this.$emit("update-size", Number(event.target.value));
+    },
+    updateX(event) {
+      this.$emit("update-x", Number(event.target.value));
+    },
+    updateY(event) {
+      this.$emit("update-y", Number(event.target.value));
+    },
+    updateZ(event) {
+      this.$emit("update-z", Number(event.target.value));
+    },
+  },
+};
+
+export default obj;
+</script>
+
+<style></style>
